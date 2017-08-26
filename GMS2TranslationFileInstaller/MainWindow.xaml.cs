1e6b67f3518e4218e81d471f33a0e43a2e61cad8
Oberon <360119124@qq.com>
2017-08-26 21:20:54 +0800

    Updated

--- GMS2TranslationFileInstaller/MainWindow.xaml.cs
+++ GMS2TranslationFileInstaller/MainWindow.xaml.cs
@@ -13,16 +13,61 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Microsoft.Win32;
+
 namespace GMS2TranslationFileInstaller
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
+
     public partial class MainWindow : Window
     {
+
+        private const string strInstallDirNotFound = "<!未找到GameMaker Studio 2的路径>";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender,EventArgs e)
+        {
+            ChBoxAutoSearch.IsChecked = true;
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ChBoxAutoSearch_Checked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\GameMakerStudio2");
+                TextInstallDir.Text = key.GetValue("Install_Dir").ToString();
+                TextInstallDir.IsEnabled = false;
+                BtnInstallDirBrowse.IsEnabled = false;
+            }
+            catch (System.IO.IOException)
+            {
+                TextInstallDir.Text = strInstallDirNotFound;
+                ChBoxAutoSearch.IsChecked = false;
+            }
+        }
+
+
+        private void ChBoxAutoSearch_Unchecked(object sender, RoutedEventArgs e)
+        {
+            BtnInstallDirBrowse.IsEnabled = true;
+            TextInstallDir.IsEnabled = true;
         }
     }
 }
