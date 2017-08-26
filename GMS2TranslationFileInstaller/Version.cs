89fbf2d4c8381ab06228ab202794e2163cdd0fbc
Oberon <360119124@qq.com>
2017-08-26 22:27:13 +0800

    Updated

--- GMS2TranslationFileInstaller/Version.cs
+++ GMS2TranslationFileInstaller/Version.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace GMS2TranslationFileInstaller
 {
@@ -20,6 +22,35 @@ namespace GMS2TranslationFileInstaller
         private int build;
         public int Build { get => build; set => build = value; }
 
+        public Version()
+        { }
+        public Version(int major,int minor,int revision,int build)
+        {
+            this.major = major;
+            this.minor = minor;
+            this.revision = revision;
+            this.build = build;
+        }
+        public Version(string strVer)
+        {
+            Regex regex = new Regex(@"(\d+)\.(\d+)\.(\d+)\.(\d+)");
+            string[] list = new string[4]; 
+            if (regex.IsMatch(strVer))
+            {
+                strVer = regex.Match(strVer).ToString();
+                list = strVer.Split('.');
+                Major = Convert.ToInt32(list[0]);
+                Minor = Convert.ToInt32(list[1]);
+                Revision = Convert.ToInt32(list[2]);
+                Build = Convert.ToInt32(list[3]);
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}.{1}.{2}.{3}",Major,Minor,Revision,Build);
+        }
+        
         public static bool operator<(Version ver1,Version ver2)
         {
             if(ver1.Major<ver2.Major)
@@ -49,7 +80,6 @@ namespace GMS2TranslationFileInstaller
             }
             return false;
         }
-
         public static bool operator>(Version ver1,Version ver2)
         {
             if (ver1.Major > ver2.Major)
@@ -80,7 +110,24 @@ namespace GMS2TranslationFileInstaller
             return false;
 
         }
+        public static bool operator==(Version ver1,Version ver2)
+        {
+            return ver1.Equals(ver2);
+        }
+        public static bool operator!=(Version ver1,Version ver2)
+        {
+            return !(ver1 == ver2);
+        }
+        public static bool operator<=(Version ver1,Version ver2)
+        {
+            return !(ver1 > ver2);
+        }
+        public static bool operator>=(Version ver1,Version ver2)
+        {
+            return !(ver1 < ver2);
+        }
 
+        
 
 
     }
